0a234c223c050b10b27ef00153d9f6346ce8f640
Luis Tamarez <luistamarez@hotmail.com>
2022-04-28 18:22:34 -0700

    Add boilerplate code     * css     * html     * JavaScript

---
+++ scripts/script.js
@@ -0,0 +1,34 @@
+// adds footer content to the page
+const footer = document.querySelector('.footer');
+const footerPara_1 = document.createElement('p');
+const footerPara_2 = document.createElement('p');
+const a = document.createElement('a');
+a.href = "https://github.com/luizerkp";
+const githubLogo = document.createElement('img');
+githubLogo.src="imgs/GitHubMarkSmall.png"
+a.appendChild(githubLogo)
+a.setAttribute('id', 'github-log');
+const date = new Date().getFullYear();
+footerPara_1.textContent = `Copyright © ${date} Luis Tamarez`
+footerPara_2.textContent = "All Rights Reserved";
+footer.appendChild(footerPara_1);
+footer.appendChild(a);
+footer.appendChild(footerPara_2)
+
+
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title
+        this.author = author
+        this.pages = pages
+        this.read = read
+    }
+    info (){
+    return `"${this.title} by ${this.author}, ${this.pages}, ${this.read}"`;
+} 
+
+}; 
+
+const hobbit = new Book("The Hobbit", "J.R.R Tolkien", "295 pages", "not read yet");
+
+console.log(hobbit.info());
