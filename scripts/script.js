bde50f00f42f8b5158a94ad17abc424a621a2baa
Luis Tamarez <luistamarez@hotmail.com>
2022-05-12 18:39:44 -0700

    Merge addBook.html page into index.html as a Modal     * Add ability to display books in lybrary to dashboard     * Merge styles sheets into style.css     * Dinamically displays books as they are added     * Hides Sample book card when first book is displayed/added

--- scripts/script.js
+++ scripts/script.js
@@ -16,109 +16,121 @@ footer.appendChild(a);
 footer.appendChild(footerPara_2)
 
 let bookLibrary = [];
+console.log(bookLibrary);
+const showAddBookForm = document.querySelector("#showAddBook");
+const modal = document.querySelector(".modal");
+const closeButton = document.querySelector(".close-button")
+
+showAddBookForm.addEventListener('click', () => {
+    modal.classList.add("show-modal");
+
+    closeButton.addEventListener('click', () => {
+        modal.classList.remove("show-modal");
+    });
+});
 
 /////////////////////////////////////////////////////////////////////////////////////
-//////////////////////////// Logic for addBook.html /////////////////////////////////
-function addBookPage() {
-    const bookForm = document.querySelector('#book-form');
-    const inputs = document.querySelectorAll('input');
-    const title = document.querySelector('#title');
-    const author = document.querySelector('#author');
-    const pages = document.querySelector('#pages');
-    const read = document.querySelector('#read');
-    const notRead = document.querySelector('#not-read');
+//////////////////////////// Logic for addBook modal ////////////////////////////////
 
-    read.addEventListener('click', (event) => {
-        checkboxHandler(event);
-    });
-    notRead.addEventListener('click', (event) => {
-        checkboxHandler(event);
-    });
+const bookForm = document.querySelector('#book-form');
+const inputs = document.querySelectorAll('input');
+const title = document.querySelector('#title');
+const author = document.querySelector('#author');
+const pages = document.querySelector('#pages');
+const read = document.querySelector('#read');
+const notRead = document.querySelector('#not-read');
 
-    // focus and blur events
-    inputs.forEach(input => {
-        input.addEventListener('focus', () => {    
-            if(!input.classList.contains('no-focus')){
-            input.classList.add('input-focus');
-            }
-        });
+read.addEventListener('click', (event) => {
+    checkboxHandler(event);
+});
+notRead.addEventListener('click', (event) => {
+    checkboxHandler(event);
+});
 
-        input.addEventListener('blur', () => {
-            input.classList.remove('input-focus');
-    });
-    });
-
-    class Book {
-        constructor(title, author, pages, read) {
-            this.title = title
-            this.author = author
-            this.pages = pages
-            this.read = read
+// focus and blur events
+inputs.forEach(input => {
+    input.addEventListener('focus', () => {    
+        if(!input.classList.contains('no-focus')){
+        input.classList.add('input-focus');
         }
-        info (){
-        return `"${this.title} by ${this.author}, ${this.pages + " pages"}, ${this.read}"`;
-        } 
-    }; 
-
-    bookForm.addEventListener('submit', (event) => {
-        event.preventDefault();
-        addBookToLibrary();
-        // redirect to the library page
     });
 
-    function addBookToLibrary() {
-        let bookReadStatus = read.checked === true ? 'read' : 'not read yet';
-        let book = new Book(title.value, author.value, pages.value, bookReadStatus);
-        bookLibrary.push(book);
-        console.log(book.info());
+    input.addEventListener('blur', () => {
+        input.classList.remove('input-focus');
+    });
+});
+
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title
+        this.author = author
+        this.pages = pages
+        this.read = read
     }
+    info (){
+    return `"${this.title} by ${this.author}, ${this.pages + " pages"}, ${this.read}"`;
+    } 
+}; 
 
-    const hobbit = new Book("The Hobbit", "J.R.R Tolkien", "295", "read");
-    const hobbit2 = new Book("The Hobbit 2", "J.R.R Tolkien", "295", "not read yet");
-    const hobbit3 = new Book("The Hobbit 3", "J.R.R Tolkien", "295", "not read yet");
-    bookLibrary.push(hobbit, hobbit2, hobbit3);
+bookForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    addBookToLibrary();
+    modal.classList.remove("show-modal");
+});
 
-    function checkboxHandler(event) {
-        if(event.target.id === 'read') {
-            read.checked = true;
-            notRead.checked = false;
-        } else {
-            read.checked = false;
-            notRead.checked = true;
-        }
-    }    
+function addBookToLibrary() {
+    let bookReadStatus = read.checked === true ? 'read' : 'not read yet';
+    let book = new Book(title.value, author.value, pages.value, bookReadStatus);
+    bookLibrary.push(book);
+    // console.log(book.info());
+    buildBookDisplay(book);
 }
 
+function checkboxHandler(event) {
+    if(event.target.id === 'read') {
+        read.checked = true;
+        notRead.checked = false;
+    } else {
+        read.checked = false;
+        notRead.checked = true;
+    }
+}    
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////// Logic for index.html /////////////////////////////////////////////////////////////////////
-function index() {
-    const cardContainer = document.getElementsByClassName(".card-container");
-    const sampleCard = document.getElementById("sample");
+////////////////////////////////////////////////// Logic for index.html non-modal content /////////////////////////////////////////////////////
 
-    // used to test if display none works as intendeded 
-    // window.addEventListener('click', () => { 
-    //     sampleCard.classList.add("hide");
-    // });
+const sampleCard = document.getElementById("sample");
 
-    // build card display... still need to complete
-    function buildBookDisplay(book){
-        const cardDiv = document.createElement("div");
-        const cardInputDiv = document.createElement("div");
-        const paraTittle = document.createElement("p");
-        const paraAuthor = document.createElement("p");
-        const paraPages = document.createElement("p");
-        const inputHeart = document.createElement("input")
-        const inputCheck = document.createElement("input")
-        const inputQueue = document.createElement("input")
-        const inputDelete = document.createElement("input")
-        
-        paraTittle.textContent = book.title;
-        paraAuthor.textContent = book.author;
-        paraPages.textContent = book.pages + " " + "pages"
+// build card display... still need to complete
+function buildBookDisplay(book){
+    // get card container
+    const cardContainer = document.getElementById("cards");
 
-    }    
-}
+    // clone sample card
+    const bookCard = sampleCard.cloneNode(true); 
+
+    // remove id attribute from cloned Node
+    bookCard.removeAttribute("id")
+
+    // Update the first 3 children nodes' text content
+    bookCard.children[0].textContent = book.title;
+    bookCard.children[1].textContent = book.author;
+    bookCard.children[2].textContent = book.pages + " " + "pages";
+
+    // console.log(
+    //     bookCard.children[0].textContent = book.title,
+    //     bookCard.children[1].textContent = book.author,
+    //     bookCard.children[2].textContent = book.pages + " " + "pages"
+    // );
+    cardContainer.appendChild(bookCard);
+
+    // if sample card is displayed hide it
+    if (!sampleCard.classList.contains("hide")) {
+        sampleCard.classList.add("hide");
+    }
+}    
+
+
