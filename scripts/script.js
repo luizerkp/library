573fca01ce7229d3bc78f9b1a50c64d9ba3fc6e7
Luis Tamarez <luistamarez@hotmail.com>
2022-05-18 23:39:06 -0700

    Add ability to remove books from the library     * Favorite/unfavorite books     * Add/remove books from queue     * Mark books as read/unread     * Still working on deleting books     * Add tooltip to see full book tittle or author name     * add elipsis to long book titlesa and author names     * input overlay to sample book card that allows user to add book to         library

--- scripts/script.js
+++ scripts/script.js
@@ -16,10 +16,12 @@ footer.appendChild(a);
 footer.appendChild(footerPara_2)
 
 let bookLibrary = [];
-console.log(bookLibrary);
+
 const showAddBookForm = document.querySelector("#showAddBook");
 const modal = document.querySelector(".modal");
 const closeButton = document.querySelector(".close-button")
+const addSymbol = document.getElementById("plus");
+const sampleCard = document.getElementById("sample");
 
 showAddBookForm.addEventListener('click', () => {
     modal.classList.add("show-modal");
@@ -29,11 +31,15 @@ showAddBookForm.addEventListener('click', () => {
     });
 });
 
+addSymbol.addEventListener('click', () => {
+    showAddBookForm.click();
+});
+
 /////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////// Logic for addBook modal ////////////////////////////////
 
 const bookForm = document.querySelector('#book-form');
-const inputs = document.querySelectorAll('input');
+const modalInputs = document.querySelectorAll('.modal-input');
 const title = document.querySelector('#title');
 const author = document.querySelector('#author');
 const pages = document.querySelector('#pages');
@@ -48,11 +54,9 @@ notRead.addEventListener('click', (event) => {
 });
 
 // focus and blur events
-inputs.forEach(input => {
+modalInputs.forEach(input => {
     input.addEventListener('focus', () => {    
-        if(!input.classList.contains('no-focus')){
         input.classList.add('input-focus');
-        }
     });
 
     input.addEventListener('blur', () => {
@@ -71,6 +75,32 @@ class Book {
     return `"${this.title} by ${this.author}, ${this.pages + " pages"}, ${this.read}"`;
     } 
 }; 
+
+// build sample books
+const lotr = new Book("The Fellowship of the Ring", "JRR Tolkien", "423", "read");
+const theWayOfKings = new Book("The Way of Kings", "Brandon Sanderson", "1007", "read");
+const theEyeOfTheWorld = new Book("The Eye of the World", "Robert Jordan", "782", "read");
+const unsouled = new Book("Unsouled", "Will Wight", "292", "read");
+const stormFront = new Book("Storm Front", "Jim Butcher", "322", "read");
+const longBookTitle = new Book("No Matter How Much You Promise to Cook or Pay the Rent You Blew It Cauze Bill Bailey Ain’t Never Coming Home Again", "Bill Bailey", "638", "not read yet");
+const longAuthorName =  new Book("Dymer", "Clive Hamilton, N. W. Clerk", "107", "not read yet");
+
+// push sample books to bookLibrary array
+bookLibrary.push(longBookTitle, longAuthorName, theWayOfKings, theEyeOfTheWorld, unsouled, stormFront, lotr);
+
+const selectIconsFunctions = {
+    'favorite': addToFavorite,
+    "read": toggleRead,
+    "queue": toggleQueue,
+    "delete": deleteBook
+}
+
+
+if (bookLibrary.length > 0) {
+    bookLibrary.forEach((book) => {
+        buildBookDisplay(book);
+    });
+}
 
 bookForm.addEventListener('submit', (event) => {
     event.preventDefault();
@@ -102,8 +132,6 @@ function checkboxHandler(event) {
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////// Logic for index.html non-modal content /////////////////////////////////////////////////////
 
-const sampleCard = document.getElementById("sample");
-
 // build card display... still need to complete
 function buildBookDisplay(book){
     // get card container
@@ -113,24 +141,85 @@ function buildBookDisplay(book){
     const bookCard = sampleCard.cloneNode(true); 
 
     // remove id attribute from cloned Node
-    bookCard.removeAttribute("id")
+    bookCard.removeAttribute("id");
+
+    // Remove plus symbol which is the first child of sample card that was cloned
+    bookCard.removeChild(bookCard.children[0]);
+
 
     // Update the first 3 children nodes' text content
-    bookCard.children[0].textContent = book.title;
-    bookCard.children[1].textContent = book.author;
-    bookCard.children[2].textContent = book.pages + " " + "pages";
-
-    // console.log(
-    //     bookCard.children[0].textContent = book.title,
-    //     bookCard.children[1].textContent = book.author,
-    //     bookCard.children[2].textContent = book.pages + " " + "pages"
-    // );
-    cardContainer.appendChild(bookCard);
-
-    // if sample card is displayed hide it
-    if (!sampleCard.classList.contains("hide")) {
-        sampleCard.classList.add("hide");
+    // Since first child was removed now .children[0] is the book title child element
+    // truncate title if it is too long and add ellipsis with a tooltip
+    if (book.title.length > 25) {
+        bookCard.children[0].textContent = book.title.slice(0, 25) + "...";
+        bookCard.children[0].dataset.title = book.title;
+    } else {
+        bookCard.children[0].textContent = book.title;
     }
+
+    // truncate author if it is too long and add ellipsis with a tooltip
+    if (book.author.length > 17) {
+        bookCard.children[2].textContent = book.author.slice(0, 20) + "...";
+        bookCard.children[2].dataset.title = book.author;
+    } else {
+        bookCard.children[2].textContent = book.author;
+    }
+
+    bookCard.children[3].textContent = book.pages + " " + "pages";
+
+    // Add class to icons
+    for (let i = 0; i < bookCard.children[4].children.length; i++){
+        bookCard.children[4].children[i].classList.add("icon-list");
+    }
+
+    if(book.read === 'read') {
+        bookCard.children[4].children[1].src = "./imgs/check-green.png";
+    }
+
+    cardContainer.prepend(bookCard);
 }    
 
+const selectIcons = document.querySelectorAll(".icon-list")
 
+selectIcons.forEach(icon => {
+    icon.addEventListener('click', (event) => {
+        selectIconsFunctions[event.target.name](event);
+    });
+});
+
+function addToFavorite(event) {
+    if(event.target.id === 'favorited') {
+        event.target.src = "./imgs/heart-plus-outline.png";
+        event.target.removeAttribute("id");
+    } else {
+        event.target.src = "./imgs/heart-red.png";
+        event.target.setAttribute("id", "favorited");
+    }
+}
+
+function toggleRead(event) {
+    if(event.target.id === 'complete') {
+        event.target.src = "./imgs/check-outline.png";
+        event.target.removeAttribute("id");
+    } else {
+        event.target.src = "./imgs/check-green.png"; 
+        event.target.setAttribute("id", "complete");
+    }
+}
+
+function toggleQueue(event) {
+    if(event.target.id === 'queued') {
+        event.target.src = "./imgs/file-document-plus-outline.png";
+        event.target.removeAttribute("id");
+    } else {
+        event.target.src = "./imgs/file-document-multiple.svg";
+        event.target.setAttribute("id", "queued");
+    }
+}
+
+function deleteBook(event) {
+    //trying to delete book from library
+    // bookLibrary.splice(bookLibrary.indexOf(event.target.parentNode.parentNode), 1);
+    event.target.parentElement.parentElement.remove();
+    console.log(bookLibrary);
+}
